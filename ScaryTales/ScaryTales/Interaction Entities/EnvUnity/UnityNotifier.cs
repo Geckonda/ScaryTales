6580b4c37801c478b608bfa3e63d4a4cb54456ff
Geckonda <Aquariusstd@yandex.ru>
2025-02-10 10:39:23 +0400

    feat(EnvUnity): (БЕЗ РЕАЛИЗАЦИИ) добавил классы для unity реализующие интерфесы IPlayerInput и INotifier

---
+++ ScaryTales/ScaryTales/Interaction Entities/EnvUnity/UnityNotifier.cs
@@ -0,0 +1,17 @@
+﻿using ScaryTales.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaryTales.Interaction_Entities.EnvUnity
+{
+    public class UnityNotifier : INotifier
+    {
+        public void Notify(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
